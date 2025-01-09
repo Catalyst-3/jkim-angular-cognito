1b97ab3aab22cff326dd724fb58a8d4fc936979b
jdk301 <justin@webvolta.com>
2025-01-09 13:02:07 -0500

    add unauthorized routing

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -10,6 +10,27 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): Observable<boolean> {
+    return this.authService.user$.pipe(
+      map((user) => {
+        if (user) {
+          return true;
+        } else {
+          console.log("Access Denied: User not logged in");
+          this.router.navigate(["/unauthorized"]);
+          return false;
+        }
+      })
+    );
+  }
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthAdminGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): Observable<boolean> {
     return this.authService.userGroups$.pipe(
       map((groups) => {
         if (groups && groups.includes("admins")) {
